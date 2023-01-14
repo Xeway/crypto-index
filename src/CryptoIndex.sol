6d87c9520cf09cc1f8a0ec9948c1736cc139c0a7
xeway <xeway@protonmail.com>
2023-01-14 22:57:02 +0100

    Fixes

--- src/CryptoIndex.sol
+++ src/CryptoIndex.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
 import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
 
-struct Token {
-    IERC20 addr;
-    // percentage en % * 100, so that it supports 2 decimal places
-    // e.g. 18.75% => 1875
-    uint percentage;
-}
-
 contract CryptoIndex {
+    struct Token {
+        address addr;
+        // percentage en % * 100, so that it supports 2 decimal places
+        // e.g. 18.75% => 1875
+        uint percentage;
+    }
+
     Token[] public tokens;
 
     mapping(address => mapping(address => uint)) public shares;
@@ -21,41 +21,45 @@ contract CryptoIndex {
 
     ISwapRouter public immutable swapRouter;
 
-    constructor(Token[] calldata _tokens, address _quoteToken, uint _amount, ISwapRouter _swapRouter) {
+    constructor(Token[] memory _tokens, address _quoteToken, uint _amount, uint24 _fee, address _swapRouter) {
         // we verify that the list of token doesn't contains the quote token
         // other it would swap the same token which throws an error
         for (uint i = 0; i < _tokens.length; ++i) {
-            if (_tokens[i] == _quoteToken) {
+            if (_tokens[i].addr == _quoteToken) {
                 _tokens[i] = _tokens[_tokens.length - 1];
-                _tokens.pop();
+                delete _tokens[_tokens.length - 1];
             }
         }
 
         tokens = _tokens;
         quoteToken = _quoteToken;
-        swapRouter = _swapRouter;
+        swapRouter = ISwapRouter(_swapRouter);
 
         if (_amount > 0) {
-            buyTokens();
+            buyTokens(_amount, _fee);
         }
     }
 
-    function buyTokens(uint _amount) public {
-        TransferHelper.safeTransferFrom(quoteToken, msg.sender, address(this), _amount);
+    function buyTokens(uint _amount, uint24 _poolFee) public {
+        address m_quoteToken = quoteToken;
+
+        // Transfer the specified amount of DAI to this contract.
+        TransferHelper.safeTransferFrom(m_quoteToken, msg.sender, address(this), _amount);
 
         ISwapRouter m_swapRouter = swapRouter;
 
-        TransferHelper.safeApprove(DAI, address(m_swapRouter), amountIn);
+        // Approve the router to spend DAI.
+        TransferHelper.safeApprove(m_quoteToken, address(m_swapRouter), _amount);
 
-        Token[] m_tokens = tokens;
+        Token[] memory m_tokens = tokens;
 
-        for (uint i = 0; i < m_tokens.length; ++i) {
-            uint amountIn = msg.value / 10000 * m_tokens[i].percentage;
+        for (uint i = 0; i < ((m_tokens[m_tokens.length - 1].addr != address(0)) ? m_tokens.length : m_tokens.length - 1); ++i) {
+            uint amountIn = _amount / 10000 * m_tokens[i].percentage;
 
             ISwapRouter.ExactInputSingleParams memory params = ISwapRouter.ExactInputSingleParams({
-                tokenIn: USDC,
+                tokenIn: m_quoteToken,
                 tokenOut: m_tokens[i].addr,
-                fee: poolFee,
+                fee: _poolFee,
                 recipient: address(this),
                 deadline: block.timestamp,
                 amountIn: amountIn,
@@ -63,7 +67,7 @@ contract CryptoIndex {
                 sqrtPriceLimitX96: 0
             });
 
-            amountOut = m_swapRouter.exactInputSingle(params);
+            uint amountOut = m_swapRouter.exactInputSingle(params);
             shares[msg.sender][m_tokens[i].addr] += amountOut;
         }
     }
