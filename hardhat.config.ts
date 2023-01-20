59b9766346f6741a16cb72ece17499b92ec17e6d
xeway <xeway@protonmail.com>
2023-01-20 19:28:26 +0100

    forked mainnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,19 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-foundry";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`
+      }
+    }
+  }
 };
 
 export default config;
