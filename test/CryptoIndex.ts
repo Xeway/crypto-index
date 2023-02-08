47834d9fd4670690fdfc3f35aef751e0e2465b15
xeway <xeway@protonmail.com>
2023-02-08 22:18:08 +0100

    Imported whales addresses

--- test/CryptoIndex.ts
+++ test/CryptoIndex.ts
@@ -2,17 +2,17 @@ import { expect } from "chai";
 import erc20ABI from "../erc20.abi.json";
 import { interfaces } from "../typechain-types/lib/v3-core/contracts";
 
-const tokenList: string[] = [
-    "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984", // UNI
-    "0x514910771AF9Ca656af840dff83E8264EcF986CA", // LINK
-    "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9", // AAVE
-    "0x85F17Cf997934a597031b2E18a9aB6ebD4B9f6a4", // NEAR
-    "0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2", // MKR
-    "0x4d224452801ACEd8B2F0aebE155379bb5D594381", // APE
-    "0xc00e94Cb662C3520282E6f5717214004A7f26888", // COMP
-    "0x111111111117dC0aa78b770fA6A738034120C302", // 1INCH
-    "0x0D8775F648430679A709E98d2b0Cb6250d2887EF", // BAT
-    "0xc944E90C64B2c07662A292be6244BDf05Cda44a7"  // GRT
+const tokenList: string[][] = [
+    ["0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984", "0x4b4e140D1f131fdaD6fb59C13AF796fD194e4135"], // UNI
+    ["0x514910771AF9Ca656af840dff83E8264EcF986CA", "0x9BBb46637A1Df7CADec2AFcA19C2920CdDCc8Db8"], // LINK
+    ["0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9", "0x4da27a545c0c5B758a6BA100e3a049001de870f5"], // AAVE
+    ["0x85F17Cf997934a597031b2E18a9aB6ebD4B9f6a4", "0x983dfBa1c0724786598Af0E63a9a6f94aAbd24A1"], // NEAR
+    ["0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2", "0x0a3f6849f78076aefaDf113F5BED87720274dDC0"], // MKR
+    ["0x4d224452801ACEd8B2F0aebE155379bb5D594381", "0x5954aB967Bc958940b7EB73ee84797Dc8a2AFbb9"], // APE
+    ["0xc00e94Cb662C3520282E6f5717214004A7f26888", "0x2775b1c75658Be0F640272CCb8c72ac986009e38"], // COMP
+    ["0x111111111117dC0aa78b770fA6A738034120C302", "0xD9b311D6678eDB14adA24829Cd9ae09fcc0F0704"], // 1INCH
+    ["0x0D8775F648430679A709E98d2b0Cb6250d2887EF", "0x6C8c6b02E7b2BE14d4fA6022Dfd6d75921D90E4E"], // BAT
+    ["0xc944E90C64B2c07662A292be6244BDf05Cda44a7", "0x32Ec7A59549b9F114c9D7d8b21891d91Ae7F2ca1"]  // GRT
 ];
 
 async function giveToken(tokenAddress: string, whales: string[], recipient: string) {
@@ -20,7 +20,16 @@ async function giveToken(tokenAddress: string, whales: string[], recipient: stri
         const impersonatedSigner = await ethers.getImpersonatedSigner(whale);
         const erc20 = new ethers.Contract(tokenAddress, erc20ABI, impersonatedSigner);
 
-        erc20.transfer(recipient, erc20.balanceOf(whale));
+        const [ETHProvider] = await ethers.getSigners();
+        await ETHProvider.sendTransaction({
+            to: impersonatedSigner.address,
+            value: ethers.utils.parseEther("10.0"),
+            gasLimit: 210000
+        });
+
+        await erc20.transfer(recipient, await erc20.balanceOf(whale));
+
+        console.log(`${tokenAddress} transferred to ${recipient}, owner's balance : ${await erc20.balanceOf(recipient)}`);
     }
 }
 
@@ -29,11 +38,10 @@ describe("CryptoIndex", function () {
         it("should deploy", async function() {
             const [owner, otherAccount] = await hre.ethers.getSigners();
 
-            // giveToken("0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9", ["0x4da27a545c0c5B758a6BA100e3a049001de870f5"], owner.address);
-
             let tokens: [string, number][] = [];
             for (let token of tokenList) {
-                tokens.push([token, 10000 / tokenList.length]);
+                await giveToken(token[0], [token[1]], owner.address);
+                tokens.push([token[0], 10000 / tokenList.length]);
             }
 
             const CryptoIndex = await hre.ethers.getContractFactory("CryptoIndex");
